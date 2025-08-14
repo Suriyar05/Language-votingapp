41f396850553895e60168e3bac31503d28f2faab
Suriyar05 <ranganathansuriya2@gmail.com>
2025-08-14 12:07:28 +0530

    Added current leader feature in Results component

--- src/Result.js
+++ src/Result.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 function Results({ languages }) {
-  // Calculate total votes
+
   const totalVotes = languages.reduce((sum, lang) => sum + lang.votes, 0);
+
+
+  let leader = null;
+  if (totalVotes > 0) {
+    const maxVotes = Math.max(...languages.map(lang => lang.votes));
+    const leaders = languages.filter(lang => lang.votes === maxVotes);
+
+    if (leaders.length === 1) {
+      leader = `${leaders[0].name} is leading with ${maxVotes} votes!`;
+    } else {
+      leader = `It's a tie between ${leaders.map(l => l.name).join(", ")} with ${maxVotes} votes each!`;
+    }
+  }
 
   return (
     <div className="results">
@@ -10,16 +23,21 @@ function Results({ languages }) {
       {totalVotes === 0 ? (
         <p>No votes yet!</p>
       ) : (
-        <ul>
-          {languages.map((lang, i) => {
-            const percentage = ((lang.votes / totalVotes) * 100).toFixed(2);
-            return (
-              <li key={i}>
-                {lang.name}: {percentage}% ({lang.votes} votes)
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <ul>
+            {languages.map((lang, i) => {
+              const percentage = ((lang.votes / totalVotes) * 100).toFixed(2);
+              return (
+                <li key={i}>
+                  {lang.name}: {percentage}% ({lang.votes} votes)
+                </li>
+              );
+            })}
+          </ul>
+          <div className="leader">
+            <strong>{leader}</strong>
+          </div>
+        </>
       )}
     </div>
   );
